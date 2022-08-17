b459da645025993ec0b633cb128166495e84615c
NjohPrince <jufredprince@gmail.com>
2022-08-17 13:01:38 +0100

    toggling suggestions menu based on selected element

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@
 
 const filterField = document.querySelector("#filter");
 const contactField = document.querySelector("#contact");
-const suggestionsList = document.querySelector(".suggestions__list");
+const suggestionsList = document.querySelector("#suggestions__list");
+const suggestions = document.querySelectorAll(".suggestion");
 
 let dataArray = {
   contact: "",
@@ -11,10 +12,25 @@ let dataArray = {
 
 const { contact, filterValue } = dataArray;
 
+// function to control visibility of the suggestions list
+const toggleSuggestionsList = () => {
+  suggestionsList.classList.toggle("show");
+};
+
+// once a suggestion is selected run we fetch the corresponding
+// details from the server and hide the suggestions list
+suggestions.forEach((suggestion, index) => {
+  suggestion.addEventListener("click", () => {
+    toggleSuggestionsList();
+  });
+});
+
+// handling filtering change events
 const getFilterValue = (e) => {
   console.log("Filter Value: ", e.target.value);
 };
 
+// handling getting the contact entered
 const getContactValue = (e) => {
   console.log("Contact: ", e.target.value);
 };
