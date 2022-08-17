9e06a6cb73b350505918dca8f732d944f9691db1
NjohPrince <jufredprince@gmail.com>
2022-08-17 12:55:14 +0100

    styles fix and selectors in js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // https://restcountries.com/v2/all - API we will be using
+
+const filterField = document.querySelector("#filter");
+const contactField = document.querySelector("#contact");
+const suggestionsList = document.querySelector(".suggestions__list");
+
+let dataArray = {
+  contact: "",
+  filterValue: "",
+};
+
+const { contact, filterValue } = dataArray;
+
+const getFilterValue = (e) => {
+  console.log("Filter Value: ", e.target.value);
+};
+
+const getContactValue = (e) => {
+  console.log("Contact: ", e.target.value);
+};
